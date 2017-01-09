7575e5d41bf252a7edef5a3d92078d4fa1955455
F0NT3 <F0NT3@outlook.com>
2017-01-09 20:56:21 +0100

    Mejorada la vista de tecnico admin

--- web/application/controllers/Tecnico_admin.php
+++ web/application/controllers/Tecnico_admin.php
@@ -6,14 +6,113 @@ class Tecnico_admin extends MY_Controller {
 
     public function __construct() {
         parent::__construct();
-        $this->load->library('plantilla');
-        $this->load->model('usuario');
+        $this->load->helper('form');
+        $this->load->helper('security'); // form_validation -> xss_clean
+        $this->load->helper('string'); // Generar contraseña aleatoria
+        $this->load->helper('descarga'); // No se usa download porque no se puede cambiar el nombre del fichero cuando se descarga
+        $this->load->library(array('form_validation', 'encryption', 'plantilla', 'upload'));
+        $this->load->model(array('usuario', 'cliente_modelo', 'tecnico_admin', 'ticket_modelo', 'tarea', 'mensaje', 'notificacion', 'factura_modelo', 'archivo'));
+        $this->upload->initialize(
+                array(
+                    'upload_path' => "./files/",
+                    'allowed_types' => "txt|pdf|gif|jpg|jpeg|png|zip|doc|docx|xls|xlsx|rar|ppt|pptm",
+                    'max_size' => "10240", // 10 MB
+                    'max_height' => "1080",
+                    'max_width' => "1920",
+                    'encrypt_name' => TRUE
+                )
+        );
     }
 
     public function index() {
         if ($this->usuario_permitido(USUARIO_TECNICO_ADMIN)) {
             $datos['titulo'] = 'Inicio';
             $this->plantilla->mostrar('tecnico_admin', 'inicio', $datos);
+        }
+    }
+
+    public function tickets() {
+        if ($this->usuario_permitido(USUARIO_TECNICO_ADMIN)) {
+            $datos['titulo'] = $this->lang->line('tickets');
+            $datos['tickets'] = $this->ticket_modelo->obtener_tickets();
+            $this->plantilla->poner_js(site_url('assets/plugins/datatables/jquery.dataTables.min.js'));
+            $this->plantilla->poner_js(site_url('assets/plugins/datatables/dataTables.bootstrap.min.js'));
+            $this->plantilla->poner_css(site_url('assets/plugins/datatables/dataTables.bootstrap.css'));
+            $this->plantilla->poner_js(site_url('assets/plugins/datatables/extensions/Responsive/js/dataTables.responsive.min.js'));
+            $this->plantilla->poner_css(site_url('assets/plugins/datatables/extensions/Responsive/css/dataTables.responsive.css'));
+            $this->plantilla->mostrar('tecnico_admin', 'tickets', $datos);
+        }
+    }
+
+    public function mensajes() {
+        if ($this->usuario_permitido(USUARIO_TECNICO_ADMIN)) {
+            $datos['titulo'] = $this->lang->line('mensajes_titulo');
+            $datos['mensajes'] = $this->mensaje->ver_mensajes_privados($this->session->userdata('id_usuario'));
+            $datos['numero_mensajes_no_vistos'] = $this->mensaje->contar_mensajes_no_vistos($this->session->userdata('id_usuario'));
+            ;
+            $datos['usuarios'] = $this->usuario->obtener_usuarios();
+            $this->plantilla->poner_css(site_url('assets/plugins/summernote/summernote.css'));
+            $this->plantilla->poner_js(site_url('assets/plugins/summernote/summernote.min.js'));
+            if ($this->session->userdata('idioma') == 'spanish') {
+                $this->plantilla->poner_js(site_url('assets/plugins/summernote/lang/summernote-es-ES.js'));
+            }
+            $this->plantilla->poner_js(site_url('assets/plugins/fastclick/fastclick.js'));
+
+            $this->plantilla->poner_css(site_url('assets/plugins/select2/select2.min.css'));
+            $this->plantilla->poner_js(site_url('assets/plugins/select2/select2.full.min.js'));
+            if ($this->session->userdata('idioma') == 'spanish') {
+                $this->plantilla->poner_js(site_url('assets/plugins/select2/i18n/es.js'));
+            }
+
+            $this->plantilla->poner_js(site_url('assets/plugins/iCheck/iCheck.min.js'));
+            $this->plantilla->poner_css(site_url('assets/plugins/iCheck/flat/blue.css'));
+
+            $this->plantilla->poner_js(site_url('assets/plugins/datatables/jquery.dataTables.min.js'));
+            $this->plantilla->poner_js(site_url('assets/plugins/datatables/dataTables.bootstrap.min.js'));
+            $this->plantilla->poner_css(site_url('assets/plugins/datatables/dataTables.bootstrap.css'));
+            $this->plantilla->poner_js(site_url('assets/plugins/datatables/extensions/Responsive/js/dataTables.responsive.min.js'));
+            $this->plantilla->poner_css(site_url('assets/plugins/datatables/extensions/Responsive/css/dataTables.responsive.css'));
+
+            $this->plantilla->poner_css(site_url('assets/plugins/bootstrap-fileinput/css/fileinput.min.css'));
+            $this->plantilla->poner_js(site_url('assets/plugins/bootstrap-fileinput/js/fileinput.min.js'));
+            if ($this->session->userdata('idioma') == 'spanish') {
+                $this->plantilla->poner_js(site_url('assets/plugins/bootstrap-fileinput/js/locales/es.js'));
+            }
+
+            $this->plantilla->poner_js('assets/plugins/bootstrap-notify/bootstrap-notify.min.js');
+            $this->plantilla->mostrar('tecnico_admin', 'mensajes', $datos);
+        }
+    }
+
+    public function ver_mensaje($id_mensaje) {
+        if ($this->usuario_permitido(USUARIO_TECNICO_ADMIN)) {
+            $datos['titulo'] = $this->lang->line('mensajes_titulo');
+            $datos['mensaje'] = $this->mensaje->obtener_mensaje($id_mensaje);
+            $this->mensaje->marcar_visto($id_mensaje);
+            $datos['numero_mensajes_no_vistos'] = $this->mensaje->contar_mensajes_no_vistos($this->session->userdata('id_usuario'));
+            $datos['usuarios'] = $this->usuario->obtener_usuarios();
+
+            $this->plantilla->poner_css(site_url('assets/plugins/summernote/summernote.css'));
+            $this->plantilla->poner_js(site_url('assets/plugins/summernote/summernote.min.js'));
+            if ($this->session->userdata('idioma') == 'spanish') {
+                $this->plantilla->poner_js(site_url('assets/plugins/summernote/lang/summernote-es-ES.js'));
+            }
+            $this->plantilla->poner_js(site_url('assets/plugins/fastclick/fastclick.js'));
+
+            $this->plantilla->poner_css(site_url('assets/plugins/select2/select2.min.css'));
+            $this->plantilla->poner_js(site_url('assets/plugins/select2/select2.full.min.js'));
+            if ($this->session->userdata('idioma') == 'spanish') {
+                $this->plantilla->poner_js(site_url('assets/plugins/select2/i18n/es.js'));
+            }
+
+            $this->plantilla->poner_css(site_url('assets/plugins/bootstrap-fileinput/css/fileinput.min.css'));
+            $this->plantilla->poner_js(site_url('assets/plugins/bootstrap-fileinput/js/fileinput.min.js'));
+            if ($this->session->userdata('idioma') == 'spanish') {
+                $this->plantilla->poner_js(site_url('assets/plugins/bootstrap-fileinput/js/locales/es.js'));
+            }
+
+            $this->plantilla->poner_js('assets/plugins/bootstrap-notify/bootstrap-notify.min.js');
+            $this->plantilla->mostrar('tecnico_admin', 'mensaje', $datos);
         }
     }
 
---
+++ web/application/views/tecnico_admin/tickets.php
@@ -0,0 +1,101 @@
+<div class="content-wrapper">
+    <!-- Cabecera -->
+    <section class="content-header">
+        <h1>
+            <?= $this->lang->line('tickets'); ?>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="<?= site_url('tecnico_admin'); ?>"><i class="fa fa-home"></i><?= $this->lang->line('inicio'); ?></a></li>
+            <li class="active"></i><?= $this->lang->line('tickets'); ?></li>
+        </ol>
+    </section>
+
+    <!-- Contenido -->
+    <section class="content">
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="box">
+
+                    <div id = "cargador" class="overlay">
+                        <i class="fa fa-refresh fa-spin"></i>
+                    </div>
+
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <?php if (isset($mensaje)) { ?>
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <div class="alert alert-success alert-dismissable">
+                                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                                        <i class="fa fa-check-circle"></i> <?= $mensaje . '.' ?>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php } ?>
+                        <a href = "javascript:history.go(0)" class="btn btn-app"><i class="fa fa-spin fa-refresh"></i><?= $this->lang->line('actualizar'); ?></a>
+                        <table data-link="" id="tickets" class="table table-bordered table-hover display nowrap">
+                            <thead>
+                                <tr>
+                                    <th><i class="fa fa-list-ol" aria-hidden="true"></i> &nbsp; <?= $this->lang->line('numero'); ?></th>
+                                    <th><i class="fa fa-user" aria-hidden="true"></i>&nbsp; <?= $this->lang->line('cliente'); ?></th>
+                                    <th><i class="fa fa-pencil" aria-hidden="true"></i> &nbsp; <?= $this->lang->line('titulo'); ?></th>
+                                    <th><i class="fa fa-calendar" aria-hidden="true"></i> &nbsp; <?= $this->lang->line('fecha'); ?> </th>
+                                    <th><i class="glyphicon glyphicon-ok" aria-hidden="true"></i>&nbsp; <?= $this->lang->line('estado'); ?></th>
+                                    <th><i class="glyphicon glyphicon-stats" aria-hidden="true"></i>&nbsp;  <?= $this->lang->line('progreso'); ?></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                                <?php
+                                foreach ($tickets as $ticket) {
+                                    echo '<tr style="cursor: pointer;">
+                                            <td><a  href="' . site_url('admin/ver_ticket/' . $ticket['id_ticket']) . '"></a>' . $ticket['id_ticket'] . '</td>
+                                            <td><a href="' . site_url('admin/ver_cliente/') . $ticket['cliente'] . '">' . $ticket['nombre_cliente'] . '</a></td>
+                                            <td>' . $ticket['titulo'] . '</td>
+                                            <td>' . date('d/m/Y H:i', strtotime($ticket['inicio'])) . '</td>
+                                            <td>';
+                                    switch ($ticket['estado']) {
+                                        case TICKET_PENDIENTE:
+                                            echo '<span class="label label-warning">' . $this->lang->line('pendiente') . '</span>';
+                                            break;
+                                        case TICKET_EN_PROCESO:
+                                            echo '<span class="label label-info">' . $this->lang->line('en_proceso') . '</span>';
+                                            break;
+                                        case TICKET_FINALIZADO:
+                                            echo '<span class="label label-success">' . $this->lang->line('finalizado') . '</span>';
+                                            break;
+                                    }
+
+                                    echo'</td>
+                                        <td> 
+                                            <div class="progress progress-xs" data-html="true" data-toggle="tooltip" data-placement="top" title="' . intval($ticket['progreso']) . ' %<br />' . $this->lang->line('tareas') . ' ' . $ticket['tareas_completadas'] . '/' . $ticket['total_tareas'] . '">
+                                              <div class="progress-bar progress-bar-green" style="width: ' . $ticket['progreso'] . '%" role="progressbar" aria-valuenow="' . $ticket['progreso'] . '" aria-valuemin="0" aria-valuemax="100">
+                                              </div>
+                                            </div>
+                                        </td>
+                                      </tr>';
+                                }
+                                ?>
+
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <th><i class="fa fa-list-ol" aria-hidden="true"></i> &nbsp; <?= $this->lang->line('numero'); ?></th>
+                                    <th><i class="fa fa-user" aria-hidden="true"></i>&nbsp; <?= $this->lang->line('cliente'); ?></th>
+                                    <th><i class="fa fa-pencil" aria-hidden="true"></i> &nbsp; <?= $this->lang->line('titulo'); ?></th>
+                                    <th><i class="fa fa-calendar" aria-hidden="true"></i> &nbsp; <?= $this->lang->line('fecha'); ?> </th>
+                                    <th><i class="glyphicon glyphicon-ok" aria-hidden="true"></i>&nbsp; <?= $this->lang->line('estado'); ?></th>
+                                    <th><i class="glyphicon glyphicon-stats" aria-hidden="true"></i>&nbsp;  <?= $this->lang->line('progreso'); ?></th>
+                                </tr>
+                            </tfoot>
+                        </table>
+
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
